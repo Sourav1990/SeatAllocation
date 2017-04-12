e9c5e700a348021987b4171bddcc5b53222feb56
Sourav Kumar Biswas <souravictory@gmail.com>
2017-04-12 22:06:19 +0530

    Started the allocation of seat using hashmap

---
+++ src/com/niit/seatallocation/Allocation.java
@@ -0,0 +1,19 @@
+package com.niit.seatallocation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Allocation {
+	public List<String> seatAllocation(int []arr)
+	{
+		  Map<Integer, String> map= new HashMap<Integer, String>();
+		map.put(1,"WS");
+		map.put(2, "MS");
+		map.put(3, "AS");
+		List<String> list= new ArrayList<String>();
+		return list;
+		
+}
+}
