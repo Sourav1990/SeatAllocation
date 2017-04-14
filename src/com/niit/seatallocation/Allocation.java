cb70001c3b1900bcb1bd182d8854d000aa11a402
Sourav Kumar Biswas <souravictory@gmail.com>
2017-04-14 13:08:25 +0530

    Done using switch case with constraints keeping in mind

--- src/com/niit/seatallocation/Allocation.java
+++ src/com/niit/seatallocation/Allocation.java
@@ -1,19 +1,39 @@
 package com.niit.seatallocation;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-public class Allocation {
-	public List<String> seatAllocation(int []arr)
-	{
-		  Map<Integer, String> map= new HashMap<Integer, String>();
-		map.put(1,"WS");
-		map.put(2, "MS");
-		map.put(3, "AS");
-		List<String> list= new ArrayList<String>();
-		return list;
-		
-}
+import java.io.*;
+public class Allocation{
+    public static void main(String[]args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        int i,n;int a[]=new int[11];
+        do
+        {
+            System.out.println("Enter the number of test cases(T)");
+            n=Integer.parseInt(in.readLine());
+            System.out.println("Enter the seat number to be checked(N)");
+            for(i=1;i<=n;i++)
+           {
+               
+               do{
+                   
+                a[i]=Integer.parseInt(in.readLine());
+            }while(!(a[i]<=108));
+        }
+        }while(!((n<10)&&(n>0)));
+        for(i=1;i<=n;i++)
+        {
+            
+            switch (a[i]%3)
+            {
+                case 1:System.out.println("WS"+" "+a[i]);
+                break;
+                case 2:System.out.println("MS"+" "+a[i]);
+                break;
+                case 0:System.out.println("AS"+" "+a[i]);
+                break;
+                default:System.out.println("error");
+            }
+        }
+    }
 }
